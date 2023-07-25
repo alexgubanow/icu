e0c182f5bcd2db561e6689c8f7ab295001e3cee6
Oleksandr Hubanov <alex@0x0h.com>
2023-07-25 15:22:49 +0300

    possible fix for sidedeck name

--- icu4c/source/tools/pkgdata/pkgdata.cpp
+++ icu4c/source/tools/pkgdata/pkgdata.cpp
@@ -1411,11 +1411,10 @@ static int32_t pkg_generateLibraryFile(const char *targetDir, const char mode, c
                 targetDir,
                 libFileNames[LIB_FILE_VERSION_TMP],
 #else
-        sprintf(cmd, "%s %s -x %s%s.x -o %s%s %s %s%s %s %s",
+        sprintf(cmd, "%s %s -x %s58.3.x -o %s%s %s %s%s %s %s",
                 pkgDataFlags[GENLIB],
                 pkgDataFlags[LDICUDTFLAGS],
-                libFileNames[LIB_FILE],
-                libFileNames[LIB_FILE_VERSION],                
+                libFileNames[LIB_FILE_OS390BATCH_VERSION],                
                 targetDir,
                 libFileNames[LIB_FILE_VERSION_TMP],
 #endif
